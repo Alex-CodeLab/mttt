f34172e15a0cdee91290a26ffd24c1673a7fe1b3
alex <dsoftware@protonmail.com>
2018-09-27 09:20:57 +0200

    0.2

--- mttt/t.py
+++ mttt/t.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
+from __future__ import print_function
 import sys
 import os
 import argparse
+
 from datetime import datetime
 import subprocess
 
@@ -34,33 +36,34 @@ class Tt():
 
     if self.action == "current":
         print("Current project: " + self.latest)
-        f = open(os.path.join(self.ttprojects, self.latest), 'r')
-        try: f.readlines()[-1]
+        f = open(os.path.join(self.ttprojects, self.latest), 'r') 
+        try: print(f.readlines()[-1]) 
         except: pass
         exit()
 
-    if self.action == "edit":
-        if 'latest' in locals():
-            subprocess.check_call([EDITOR, os.path.join(self.ttprojects, self.data)])           
-            exit()
-        print("no projects yet. To create a project: start project:task  ")
 
-    if ':' in self.data:
-        self.project = self.data.split(':')[0]
-        self.content = self.data.split(':')[1]
+    if self.data:
+
+        if ':' in self.data:
+            self.project = self.data.split(':')[0]
+            self.content = self.data.split(':')[1]
+
+        else:
+            self.content= self.data
+            if hasattr(self,'latest') :
+                self.project = self.latest
+            else:
+                try:
+                    project = input("Project name: ({})".format(self.latest))
+                except:
+                    project = input("New Project name: ")
+                self.project = project
+            if self.action == "start":
+                self.project = self.data
 
     else:
-        self.content= self.data
-        if hasattr(self,'latest') :
-            self.project = self.latest
-        else:
-            try:
-                project = input("Project name: ({})".format(self.latest))
-            except:
-                project = input("New Project name: ")
-            self.project = project
-        if self.action == "start":
-            self.project = self.data
+        self.project = self.latest
+        self.content = ''
 
     self.file = open(os.path.join(self.ttprojects, self.project), "a")
 
@@ -77,22 +80,44 @@ class Tt():
     self.file.write("{} {} {}\n".format( self.nowstr, "> ", self.content ))
 
   def add(self):
+    
     self.file.write("{} {} {}\n".format( self.nowstr,"+ ", self.content ))
 
   def end(self):
-    self.file.write("{} {} {} \n".format( self.nowstr, "- ", self.content ))
+    self.file.write("{} {} {} \n".format( self.nowstr, "< ", self.content if not None else "...end"))
 
+
+  def edit(self):
+    if self.data:
+        subprocess.check_call([EDITOR, os.path.join(self.ttprojects, self.data)])           
+        exit()
+    if self.project:
+        subprocess.check_call([EDITOR, os.path.join(self.ttprojects, self.project)])           
+        exit()
+
+    print("no projects yet. To create a project: start project:task  ")    
+
+  def switch(self):
+    if self.data:
+       open(os.path.join(self.ttprojects, self.data), 'a').close()
+       print("current: {}".format(self.data))
+       f = open(os.path.join(self.ttprojects, self.data), 'r') 
+       try: 
+            print(f.readlines()[-2])
+            print(f.readlines()[-1])
+       except: pass
+       f.close()      
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser( formatter_class=argparse.RawTextHelpFormatter,
              description=  'Minimalist command-line time tracking application.')
-    parser.add_argument( 'action' , type=str, help = 'options: start, stop, projects, report, current, edit')
+    parser.add_argument( 'action' , type=str, help = 'options: start, end, projects, report, current, switch, edit')
     parser.add_argument( 'data', nargs='?',type=str, help = 'task or project:task')
 
     args = parser.parse_args()
 
-    actions = ['start','add','end','projects','report', 'current', 'edit']
+    actions = ['start','add','end','projects','report', 'current','switch' ,'edit']
     if args.action not in actions:
        parser.print_help()
        parser.exit()
